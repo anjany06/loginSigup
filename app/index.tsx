a076f5814ee879303df93f67a0cb8dabc45fbc31
Anjany Pandey <anjany.pandey06@gmail.com>
2025-05-25 23:50:14 +0530

    refactor: remove unused components and files; add basic login and signup screens

---
+++ app/index.tsx
@@ -0,0 +1,16 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View>
+      <Text
+        style={{
+          fontSize: 20,
+          fontWeight: "700",
+        }}
+      >
+        Hello Welcome to CodSOd
+      </Text>
+    </View>
+  );
+}
